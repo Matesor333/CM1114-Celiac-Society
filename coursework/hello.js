d0a4e4b9ac2cbd7360264ff1ab0d4089427c887c
Matus ILLITH <113976819+Matesor333@users.noreply.github.com>
2024-03-18 09:22:36 +0000

    Add files via upload

--- coursework/hello.js
+++ coursework/hello.js
@@ -1,6 +1,35 @@
+var starsExpress = document.querySelectorAll('.starExpress');
+var ratingExpress = 0;
+
+function setRatingExpress(ratingValue) {
+    ratingExpress = ratingValue;
+
+    starsExpress.forEach((starExpress, index) => {
+        if (ratingExpress > index) {
+            starExpress.classList.add('filled');
+        } else {
+            starExpress.classList.remove('filled');
+        }
+    });
+}
+
+var starsAmarone = document.querySelectorAll('.starAmarone');
+var ratingAmarone = 0;
+function setRatingAmarone(ratingValue) {
+    ratingAmarone = ratingValue;
+
+    starsAmarone.forEach((starAmarone, index) => {
+        if (ratingAmarone > index) {
+            starAmarone.classList.add('filled');
+        } else {
+            starAmarone.classList.remove('filled');
+        }
+    });
+}
+
+
 var stars = document.querySelectorAll('.star');
 var rating = 0;
-
 function setRating(ratingValue) {
     rating = ratingValue;
 
